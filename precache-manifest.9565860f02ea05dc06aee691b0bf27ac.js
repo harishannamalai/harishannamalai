f915beca20e65bfa0540b4ea409fd935f79553d4
Harish Annamalai <cerium11@gmail.com>
2020-07-04 23:01:47 +0530

    Initial Version

---
+++ precache-manifest.9565860f02ea05dc06aee691b0bf27ac.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "543cb931ecd312105bd76455bb7fad94",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc95dc8a0f06a4a1f4fa",
+    "url": "/static/css/2.73524e87.chunk.css"
+  },
+  {
+    "revision": "619d67f74a4ea205428e",
+    "url": "/static/css/main.9c5f566a.chunk.css"
+  },
+  {
+    "revision": "fc95dc8a0f06a4a1f4fa",
+    "url": "/static/js/2.279b423d.chunk.js"
+  },
+  {
+    "revision": "619d67f74a4ea205428e",
+    "url": "/static/js/main.93496179.chunk.js"
+  },
+  {
+    "revision": "09aa04d195271a017b4f",
+    "url": "/static/js/runtime-main.1b585973.js"
+  },
+  {
+    "revision": "0f5483817fca514f027cf5e619fa8a25",
+    "url": "/static/media/docker.0f548381.png"
+  },
+  {
+    "revision": "7a32a3b7662f04cfb96dfca258b100a7",
+    "url": "/static/media/harish_background_new.7a32a3b7.png"
+  },
+  {
+    "revision": "2e976b756f081045dec1c10160f9d4a4",
+    "url": "/static/media/harish_self_background.2e976b75.png"
+  },
+  {
+    "revision": "cac1f4afd1689c451dec49ebe540b441",
+    "url": "/static/media/java.cac1f4af.png"
+  },
+  {
+    "revision": "0e25b327b0f30bbf0e9119fc4be2b10c",
+    "url": "/static/media/kubernetes.0e25b327.png"
+  },
+  {
+    "revision": "5df7cd233ca278c2cca4c2323b452d02",
+    "url": "/static/media/react.5df7cd23.png"
+  },
+  {
+    "revision": "9461d892bd6742e1ce09b106c4aa53bf",
+    "url": "/static/media/springboot.9461d892.png"
+  }
+]);
